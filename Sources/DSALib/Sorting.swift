436924e61f8f73f7d52f99c292e5ae778eeb5fcc
Milan <milanaiub@gmail.com>
2024-05-11 17:41:40 +0900

    Test case update & fix edge cases

--- Sources/DSALib/Sorting.swift
+++ Sources/DSALib/Sorting.swift
@@ -1,18 +1,92 @@
 import Foundation
 
 struct Sorting {
+    /// Sort int array
+    /// Select a pivot 
+    /// divide elements smaller than pivot in left and largers in right
+    /// Continue until sub array contains only one element
+    /// Merge sorted subarrays
+    /// Time Complexity: nlog(n)
+    /// Space Complexity: log(n)
+
     func quickSort(elements: [Int]) -> [Int] {
         if elements.count <= 1 {
             return elements
         }
-
-        let pivot: Int = elements.count / 2
+        let midValue = elements.count / 2
+        let pivot: Int = elements[midValue]
         var left  = [Int]()
         var right = [Int]()
 
         for element in elements {
-            element < pivot ? left.append(element) : right.append(element)
+            if element < pivot { left.append(element) }
+            if element > pivot { right.append(element) }
         }
         return quickSort(elements: left) + [pivot] + quickSort(elements: right)
+    }
+
+    private func partition(arr: inout [Int], low: Int, high: Int) -> Int {
+        let pivot = arr[high]
+        var i = low - 1
+        for index in low...high {
+            if arr[index] < pivot {
+                i += 1
+                arr.swapAt(index, i)
+            }
+        }
+        arr.swapAt(i + 1, high)
+        return i + 1
+    }
+    
+    func quickSortInPlace(arr: inout [Int], low: Int, high: Int) {
+        if low < high {
+            let pivot = partition(arr: &arr, low: low, high: high)
+            
+            quickSortInPlace(arr: &arr, low: low, high: pivot-1)
+            quickSortInPlace(arr: &arr, low: pivot + 1, high: high)
+        }
+    }
+    
+    /// Merge sort
+    ///
+    private func merge(left: [Int], right: [Int]) -> [Int] {
+        var mergedArray = [Int]()
+        
+        var leftIndex = 0
+        var rightIndex = 0
+        
+        while leftIndex < left.count && rightIndex < right.count {
+            if left[leftIndex] < right[rightIndex] {
+                mergedArray.append(left[leftIndex])
+                leftIndex += 1
+            } else {
+                mergedArray.append(right[rightIndex])
+                rightIndex += 1
+            }
+        }
+        
+        while leftIndex < left.count {
+            mergedArray.append(left[leftIndex])
+            leftIndex += 1
+        }
+        
+        while rightIndex < right.count {
+            mergedArray.append(right[rightIndex])
+            rightIndex += 1
+        }
+        
+        return mergedArray
+    }
+    
+    func mergeSort(list: [Int]) -> [Int] {
+        if list.count <= 1 {
+            return list
+        }
+        
+        let midValue = list.count / 2
+        let left = mergeSort(list: Array(list[..<midValue]))
+        let right = mergeSort(list: Array(list[midValue...]))
+        
+        return merge(left: left, right: right)
     }
 }
--- Tests/DSALibTests/SortingTests.swift
+++ Tests/DSALibTests/SortingTests.swift
@@ -11,13 +11,62 @@ import XCTest
 final class SortingTests: XCTestCase {
     let sut = Sorting()
 
-    func testSortingSingleEelement() throws {
+    func testQuickSortingSingleEelement() throws {
         let result = sut.quickSort(elements: [1])
 
         XCTAssertEqual([1], result)
     }
+    
+    func testQuickSort() throws {
+        let result = sut.quickSort(elements: [193, 8, 849, 92, 29, 222, 555, 938])
+        
+        XCTAssertEqual([8, 29, 92, 193, 222, 555, 849, 938], result)
+    }
+    
+    func testMergeSort() throws {
+        let elements = [193, 8, 849, 92, 29, 222, 555, 938]
+        let sorted = sut.mergeSort(list: elements)
+        
+        XCTAssertEqual([8, 29, 92, 193, 222, 555, 849, 938], sorted)
+    }
+    
+    func testMergeSortingSingleEelement() throws {
+        let result = sut.mergeSort(list: [1])
+
+        XCTAssertEqual([1], result)
+    }
+    
+    func testInPlaceQuickSortingSingleEelement() throws {
+        var elements = [193]
+        sut.quickSortInPlace(arr: &elements, low: 0, high: elements.count - 1)
+
+        XCTAssertEqual([193], elements)
+    }
+    
+    func testQuickSortInPlace() throws {
+        var elements = [193, 8, 849, 92, 29, 222, 555, 938]
+        sut.quickSortInPlace(arr: &elements, low: 0, high: elements.count - 1)
+        
+        XCTAssertEqual([8, 29, 92, 193, 222, 555, 849, 938], elements)
+    }
+    
+    func testEmptyArraySorting() throws {
+        /// QuickSort
+        let result = sut.quickSort(elements: [])
+        XCTAssertEqual([], result)
+        
+        /// quickSortInPlace
+        var elements = [Int]()
+        sut.quickSortInPlace(arr: &elements, low: 0, high: 0)
+        XCTAssertEqual([], elements)
+        
+        /// Merge sort
+        let sorted = sut.mergeSort(list: [])
+        XCTAssertEqual([], sorted)
+    }
 
     static var allTests = [
-        ("testSortingSingleEelement", testSortingSingleEelement)
+        ("testQuickSortingSingleEelement", testQuickSortingSingleEelement),
+        ("testQuickSort", testQuickSort)
     ]
 }
